d12108e4baa4a28bd4a39ac8e2c955101a7957d5
Areaf <darkgulliotine7@gmail.com>
2023-08-19 14:15:15 -0400

    Common Footer Page across all pages

---
+++ sub-views/footer.php
@@ -0,0 +1,26 @@
+<style>
+  footer {
+    position: relative;
+  }
+</style>
+<footer class="mt-5 mb-5">
+  <em>
+    <h5 class="text-light text-center p-0 brand mt-2">
+      <img src="assets/images/airtic.png" height="40px" width="40px" alt="">
+      Aremar Airways
+    </h5>
+  </em>
+  <div class="text-light text-center">&copy;
+    <?php echo date('Y') ?> - Developed By Areaf and Maria
+  </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+  integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+  integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
